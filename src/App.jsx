f98e1e97e79b09fcabd00607c6a7b1f007078a99
jiseon.lee <aloc0107@naver.com>
2026-03-17 19:37:28 +0900

    글 추가, 글 삭제, 컴포넌트 생성 및 props

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
+
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [postTitle, postTitleChange] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [like, likeChange] = useState([0,0,0]);
+  let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [input, inputChange] = useState('');
+
+
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <div className="App">       
+          <div className='black-nav'>
+            <h4 style={{color:'#fff', fontSize:'16px'}}>React</h4>
+          </div>
 
-      <div className="ticks"></div>
+          <button onClick={()=>{
+            let copy = [...postTitle];
+            copy[0] = '여자 코트 추천';
+            postTitleChange(copy)
+          }}>첫글 바꾸기 버튼</button>
+          
+          <button onClick={()=>{
+            let copy = [...postTitle];
+            copy.sort((a, b) => a.localeCompare(b));
+            postTitleChange(copy);
+          }}>가나다순정렬</button>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        
+          {
+           postTitle.map(function(item,idx){
+              return (
+                 <div className='list' key={idx}>
+                    <h4 onClick={()=> {
+                      setModal(!modal);
+                      setTitle(idx);
+                    }}>{ postTitle[idx] } 
+                      <span onClick={
+                        (e)=>{
+                          e.stopPropagation(); 
+                          let copy = [...like];
+                          copy[idx] = copy[idx]+1;
+                          likeChange(copy)
+                        }
+                      }>👍</span> { like[idx] }                       
+                    </h4>
+                    <p>2월 17일 발행</p>
+                    <button onClick={()=>{
+                      let copyTitle = [...postTitle];
+                      copyTitle.splice(idx, 1);
+                      postTitleChange(copyTitle);
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+                      let copyLike = [...like];
+                      copyLike.splice(idx,1);
+                      likeChange(copyLike);
+                    }}>글삭제</button>
+                  </div>
+              )
+            })
+          }
+
+        <input type="text" onChange={(e)=>{
+          inputChange(e.target.value);
+          console.log(input);
+        }}/>
+        <button onClick={()=>{
+          let copy = [...postTitle];
+          copy.unshift(input);
+          postTitleChange(copy);
+        }}>글추가</button>
+
+        {
+          modal == true ? <Modal postTitle={postTitle} postTitleChange={postTitleChange} title={title} /> : null          
+        }
+
+
+      </div>
     </>
+  )
+}
+
+
+
+function Modal(props){
+  return(
+    <div className='modal' >
+      <h4>{props.postTitle[props.title]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button onClick={
+        ()=>{
+          let copy = [...props.postTitle];
+          copy[0] = "여자코트 추천";
+          props.postTitleChange(copy);
+        }
+      }>글수정</button>
+    </div>
   )
 }
 
